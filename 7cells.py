adc211e995496b69e7b9e81e2358eddbedbdddec
rravell <46346189+rravell@users.noreply.github.com>
2021-04-09 09:01:09 +0200

    Update 7cells.py

--- 7cells.py
+++ 7cells.py
@@ -379,8 +379,7 @@ def plot_iterations(opt_result):
     
     ax.plot(tlist,np.transpose(EEnergy))
     plt.legend()
-    plt.show(fig)
-    plt.savefig('ElectricField7cells.png')
+    fig.savefig('ElectricField7cells.png')
 
 # NBVAL_IGNORE_OUTPUT
 # the DensityMatrixODEPropagator is not sufficiently exact to guarantee that
@@ -453,14 +452,13 @@ for i in range(0,nt):
   Ergotropy[i]=-np.real(np.matrix.trace(omega*np.dot(np.array([[0, 0], [0, 1]]),(Rho_fCell7-FinalStateCell7))))-np.real(np.matrix.trace(omega*np.dot(np.array([[0, 0], [0, 1]]),(Rho_fCell6-FinalStateCell6))))-np.real(np.matrix.trace(omega*np.dot(np.array([[0, 0], [0, 1]]),(Rho_fCell5-FinalStateCell5))))-np.real(np.matrix.trace(omega*np.dot(np.array([[0, 0], [0, 1]]),(Rho_fCell4-FinalStateCell4))))-np.real(np.matrix.trace(omega*np.dot(np.array([[0, 0], [0, 1]]),(Rho_fCell3-FinalStateCell3))))-np.real(np.matrix.trace(omega*np.dot(np.array([[0, 0], [0, 1]]),(Rho_fCell1-FinalStateCell1))))-np.real(np.matrix.trace(omega*np.dot(np.array([[0, 0], [0, 1]]),(Rho_fCell2-FinalStateCell2))))
   time[i]=(T/nt)*i
 
-
-plt.plot(time,Energy,label='Energy')
-plt.plot(time,Ergotropy,label='Ergotropy')
-plt.xlabel("time")
-plt.ylabel("Energy, Ergotropy")
-plt.legend()  
-plt.show()
-plt.savefig('EnergyvsErgotropy7cells.png')
+fig2, ax1 =plt.subplots()
+ax1.plot(time,Energy,label='Energy')
+ax1.plot(time,Ergotropy,label='Ergotropy')
+ax1.set_xlabel("time")
+ax1.set_ylabel("Energy, Ergotropy")
+ax1.legend()  
+fig2.savefig('EnergyvsErgotropy7cells.png')
 
 
 """## Simulate the dynamics of the optimized field
@@ -473,5 +471,6 @@ oscillations between qubit and TLS ground state such that the qubit ground
 state is maximally populated at final time $T$.
 """
 
-print(Ergotropy[nt-1]/Energy[nt-1])
-print(Ergotropy[nt-1])
+file1 = open("Percentage7cells.txt","w")
+file1.write(repr(Ergotropy[nt-1]/Energy[nt-1]))
+file1.close()
