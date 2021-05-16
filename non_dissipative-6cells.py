89f746634d0c371c814e6a496523f0531223a025
rravell <46346189+rravell@users.noreply.github.com>
2021-05-16 11:35:05 +0200

    Update non_dissipative-6cells.py

--- non_dissipative-6cells.py
+++ non_dissipative-6cells.py
@@ -171,6 +171,7 @@ def plot_iterations(opt_result):
     ax.plot(tlist,np.transpose(EEnergy))
     plt.legend()
     plt.show(fig)
+    fig.savefig('Energyofthefield6cells.png')
     
     
     
@@ -258,12 +259,13 @@ plt.legend()
 plt.show()
 
 def plot_pulse(pulse, tlist):
-    fig, ax = plt.subplots()
+    fig2, ax = plt.subplots()
     if callable(pulse):
         pulse = np.array([pulse(t, args=None) for t in tlist])
     ax.plot(tlist, pulse)
     ax.set_xlabel('Time')
     ax.set_ylabel('Pulse Amplitude')
     plt.show(fig)
+    fig2.savefig('EFieldfor6cells.png')
 
 plot_pulse(opt_result.optimized_controls[0], tlist)
